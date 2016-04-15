6432a423834ddaca76083d9b915f615cd83627da
Melissa Lopez <mel501@nyu.edu>
2016-04-15 18:14:16 -0400

    Optimized

--- level_0.c
+++ level_0.c
@@ -1,5 +1,5 @@
 /*
- * author: 
+ * author: Melissa Lopez
  * 
  */
   
@@ -25,7 +25,6 @@ int level_0( int * B, int x, int y )
     for(c = 0; c < N; c++)
         for(r = 0; r < N; r++)
             B[ r*N + c ] = 2*(B[ r*N + c ] + 2);
-
     
     return  B[ x*N + y ];
 }
@@ -35,8 +34,11 @@ int level_0_opt( int * B, int x, int y )
 {
 	if (!B) return -1; 
 	
+    int c, r; 
     
-
+    for(r = 0; r < N; r++)
+        for(c = 0; c < N; c++)
+            B[ r*N + c ] = 2*(B[ r*N + c ] + 2);
     
     return  B[ x*N + y ];
 }
