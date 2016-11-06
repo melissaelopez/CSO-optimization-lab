1f54e05edde6f191305fe95d050d45181f7cfed5
Melissa Lopez <mel501@nyu.edu>
2016-11-06 16:54:43 -0500

    Update level_0.c

--- level_0.c
+++ level_0.c
@@ -8,7 +8,6 @@
 
 #include "proj3.h"
 
-
 // Implement a C function called level_0_opt. It should perform the same 
 // task as the implemented level_0 function. 
 // In the comments for the function describe why your changes make the
@@ -16,8 +15,7 @@
 
 // Note: this is a warmup level. It will not be graded. 
 
-int level_0( int * B, int x, int y )
-{
+int level_0( int * B, int x, int y ){
 	if (!B) return -1; 
 	
     int c, r; 
@@ -29,9 +27,7 @@ int level_0( int * B, int x, int y )
     return  B[ x*N + y ];
 }
 
-
-int level_0_opt( int * B, int x, int y )
-{
+int level_0_opt( int * B, int x, int y ){
 	if (!B) return -1; 
 	
     int c, r; 
